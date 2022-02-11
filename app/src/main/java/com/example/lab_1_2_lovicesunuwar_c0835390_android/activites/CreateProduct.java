ff0fa32426ba080210e76976a48ce1b3061056ca
Lovice01 <c0835390@mylambton.ca>
2022-02-10 22:08:48 -0500

    labtest 1 and 2

--- app/src/main/java/com/example/lab_1_2_lovicesunuwar_c0835390_android/activites/CreateProduct.java
+++
@@ -1,6 +0,0 @@
-package com.example.lab_1_2_lovicesunuwar_c0835390_android.activites;
-
-public class CreateProduct {
-
-
-}
--- app/src/main/java/com/example/lab_1_2_lovicesunuwar_c0835390_android/entities/Product.java
+++
@@ -1,5 +0,0 @@
-package com.example.lab_1_2_lovicesunuwar_c0835390_android.entities;
-
-public class Product {
-    
-}
